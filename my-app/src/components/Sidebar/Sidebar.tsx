9e5a6e1fc04b1247386fda03e99fe2e5c7f4da07
csm <141980543+csm009177@users.noreply.github.com>
2025-11-26 23:39:54 +0900

    index.tsx - 진입점 / Sidebar.tsx - 컴포넌트

---
+++ my-app/src/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,16 @@
+const Sidebar = () => {
+  return (
+    <aside className="sidebar">
+      <h2>Sidebar</h2>
+      <nav>
+        <ul>
+          <li>메뉴 1</li>
+          <li>메뉴 2</li>
+          <li>메뉴 3</li>
+        </ul>
+      </nav>
+    </aside>
+  )
+}
+
+export default Sidebar
